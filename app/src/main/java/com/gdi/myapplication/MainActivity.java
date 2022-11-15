1e25e0dcba255e81faa7eae8f047522a8b994a7d
jaseo <jinaseo@gdi-solution.com>
2022-11-15 13:31:31 +0900

    [INIT] Git 연동 테스트

--- app/src/main/java/com/gdi/myapplication/MainActivity.java
+++ app/src/main/java/com/gdi/myapplication/MainActivity.java
@@ -8,14 +8,22 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
+    private Boolean testYn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        testYn = true;
+
         tv = findViewById(R.id.tv_hello);
-        tv.setText(getString(R.string.test_text));
+
+        if(testYn) {
+            tv.setText(getString(R.string.git_test_text));
+        } else {
+            tv.setText(getString(R.string.test_text));
+        }
 
     }
 }
