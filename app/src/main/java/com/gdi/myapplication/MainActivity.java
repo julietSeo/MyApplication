255e4f3e625dffec2fd498b5960049172bab1bbf
Jina Seo <ja.suh@inzisoft.com>
2022-11-18 18:07:30 +0900

    [BLE] <proceeding> 블루투스 activating

--- app/src/main/java/com/gdi/myapplication/MainActivity.java
+++ app/src/main/java/com/gdi/myapplication/MainActivity.java
@@ -1,14 +1,15 @@
 package com.gdi.myapplication;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,18 +19,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Collection;
-import java.util.Map;
 
 
-
+@RequiresApi(api = Build.VERSION_CODES.S)
 public class MainActivity extends AppCompatActivity {
 
-    private final String[] PERMISSIONS = {Manifest.permission.BLUETOOTH_CONNECT/*, Manifest.permission.BLUETOOTH_SCAN*/};
-    private final int BLE_ENABLE = 100;
+    private final String[] PERMISSIONS = {Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN};
+    private final int REQUEST_BLE_ENABLE = 100;
+
 
     private TextView tv2;
     private Button btnReceive;
     private TextView receivedData;
+
+    private BluetoothManager bluetoothManager;
+    private BluetoothAdapter bluetoothAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,50 +46,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            checkEssentialPermissions();
+        // 1. check Permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_DENIED) {
+//            ActivityCompat.requestPermissions(this, PERMISSIONS, BLE_ENABLE);
+            requestPermissionLaunchers.launch(PERMISSIONS);
         }
 
-    }
+        // 2. check if device supports Bluetooth
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null) {
+            Toast.makeText(this, "Device doesn't support Bluetooth", Toast.LENGTH_SHORT).show();
+        }
 
-
-    private void checkEssentialPermissions() {
-        int checkPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT);
-        int checkPermissionB = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN);
-
-        if(checkPermission < 0) {
-            ActivityCompat.requestPermissions(this, PERMISSIONS, BLE_ENABLE);
-//            requestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT);
-        } else {
-            Toast.makeText(this, "Ready to start!", Toast.LENGTH_SHORT).show();
+        // 3. activate Bluetooth if it is deactivated
+        if(!bluetoothAdapter.enable()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            requestBleLauncher.launch(intent);
         }
     }
 
     /**
-     * Request Simple Permission
+     * Request Bluetooth Enabled
      */
-    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-
-        if(isGranted) {
-            Log.d("[Check granted]", "isGranted = true");
+    private final ActivityResultLauncher<Intent> requestBleLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if(result.getResultCode() == RESULT_OK) {
+            Toast.makeText(this, "Bluetooth is deactivated", Toast.LENGTH_SHORT).show();
         } else {
-            Log.d("[Check granted]", "isGranted = false");
+            Toast.makeText(this, "Bluetooth is deactivated", Toast.LENGTH_SHORT).show();
         }
+
     });
 
     /**
      * Request Multiple Permissions
      */
-//    private final ActivityResultLauncher<String[]> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
-//        Collection<Boolean> values = isGranted.values();
-//        for(Boolean v : values) {
-//            if(v) {
-//                Log.d("[Check permissions granted]", "isGranted = true");
-//            } else {
-//                Log.d("[Check permissions granted]", "isGranted = false");
-//            }
-//        }
-//    });
+    private final ActivityResultLauncher<String[]> requestPermissionLaunchers = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
+        Collection<Boolean> values = isGranted.values();
+        for (Boolean v : values) {
+            if (v) {
+                Log.d("[Check granted]", "isGranted = true");
+            } else {
+                Log.d("[Check granted]", "isGranted = false");
+            }
+        }
+    });
 
     private void initLayout() {
 
